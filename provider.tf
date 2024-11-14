42d37af1b8615e998665ce711637b1d78d0c11f5
Rajani45 <163117857+Rajani45@users.noreply.github.com>
2024-11-14 15:14:38 +0000

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    commercetools = {
+      source = "labd/commercetools"
+      version = "1.16.0"
+    }
+  }
+}
+
+provider "commercetools" {
+  # Configuration options
+  # client_id     = "OJ0FbNgxAEnhEOW4VMTD33UM"
+  # client_secret = "pT4hZceyb29ZA6a8CeCJMlqgMyKSVE9F"
+  # project_key   = "naga-demo-project1"
+  scopes        = "manage_project:hcl-devops manage_api_clients:hcl-devops view_api_clients:hcl-devops view_audit_log:hcl-devops"
+  # api_url       = "https://api.us-central1.gcp.commercetools.com"
+  token_url     = "https://auth.us-central1.gcp.commercetools.com"  
+}
